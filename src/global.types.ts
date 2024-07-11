d7126eea1d9b0855904508ade70779c399635186
richeek45 <richeek45@gmail.com>
2024-07-11 18:47:54 +0530

    adding types for the mock data

---
+++ src/global.types.ts
@@ -0,0 +1,43 @@
+export enum RepoActions {
+  PR_OPEN = "PR Open",
+  PR_MERGED = "PR Merged",
+  COMMITS = "Commits",
+  PR_REVIEWED = "PR Reviewed",
+  PR_COMMENTS = "PR Comments",
+  INCIDENT_ALERTS = "Incident Alerts",
+  INCIDENT_RESOLVED = "Incidents Resolved"
+}
+
+export interface Activity {
+  label: RepoActions;
+  fillColor: string;
+}
+
+interface ActiveDays {
+  days: number;
+  insight: string[];
+  isBurnOut: boolean;
+}
+
+export interface TotalActivity {
+  name: RepoActions;
+  value: string;
+}
+
+interface Items {
+  count: string;
+  fillColor: string;
+  label: RepoActions;
+}
+
+interface DayWiseActivity {
+  date: Date;
+  items: { children: Items[]}
+}
+
+export interface Row {
+  name: string;
+  totalActivity: TotalActivity[];
+  activeDays: ActiveDays;
+  dayWiseActivity: DayWiseActivity[];
+}
