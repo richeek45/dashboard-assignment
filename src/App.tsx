1a308cdd1380fb6a63a8bb9cfc18d0824da41b83
richeek45 <richeek45@gmail.com>
2024-07-13 02:32:31 +0530

    adding text to the donut charts

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
         </div>
         <div>
           
-          {data && <Charts rowData={data} activityData={activityData} />}
+          {data && activityData && <Charts rowData={data} activityData={activityData} />}
         </div>
       </div>
     </div>
--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -76,6 +76,24 @@ const Charts = ({ rowData, activityData } : { rowData: Row[], activityData: Acti
         .attr("class", "arc")
         .style("stroke-width", "1px")
 
+        svg2.append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 16)
+        .attr("text-anchor", "middle")
+        .selectAll('arc')
+        .data(pie(sampleData))
+        .join("text")
+        .attr("transform", d => `translate(${arc.centroid(d as unknown as d3.DefaultArcObject)})`)
+        .call(text => text.append("tspan")
+            .attr("y", "-0.4em")
+            .attr("font-weight", "normal")
+            .text(d => d.data.name))
+        .call(text => text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan")
+            .attr("x", 0)
+            .attr("y", "0.7em")
+            .attr("fill-opacity", 0.7)
+            .text(d => d.data.value.toLocaleString()));
+
 
       }
       
