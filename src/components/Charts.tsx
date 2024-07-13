51b73ff17525efd95a829ad54d1bb3475208c46a
richeek45 <richeek45@gmail.com>
2024-07-14 03:53:54 +0530

    added dayWiseActivity chart based on action type

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,20 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any */
-import { Activity, Row, TotalActivity } from "@/global.types";
+import { Activity, DayWiseActivity, Row, TotalActivity } from "@/global.types";
 import * as d3 from "d3";
 import { useEffect, useRef, useState } from "react";
 import BarChart from "./BarChart";
 import DonutChart from "./DonutChart";
+import DayWiseActivityChart from "./DayWiseActivityChart";
 
 export const width = 360, height = 300;
 
 const Charts = ({ rowData, activityData } : { rowData: Row[], activityData: Activity[] }) => { 
   const [totalActivity, setTotalActivity] = useState<TotalActivity[] | null>(null);
+  const [dayWiseActivity, setDayWiseActivity] = useState<DayWiseActivity[] | null>(null);
 
   useEffect(() => {
     // @ts-expect-error -> error
     d3.json("../../sample-data.json").then(({ data }: { AuthorWorklog: { rows: Rows[]}}) => {  
       const sampleData: TotalActivity[] = data.AuthorWorklog.rows[0].totalActivity;
+      const dayWiseActivity: DayWiseActivity[] = data.AuthorWorklog.rows[0].dayWiseActivity;
       setTotalActivity(sampleData);
+      setDayWiseActivity(dayWiseActivity);
     })
 
   }, [])
@@ -22,7 +26,7 @@ const Charts = ({ rowData, activityData } : { rowData: Row[], activityData: Acti
   return <div className="flex p-10 justify-end gap-10">
     <div></div>
     <div className="drop-shadow-md border-gray-300 border-2">
-      <svg id="daywise"></svg>
+      {dayWiseActivity && <DayWiseActivityChart dayWiseActivity={dayWiseActivity} />}
     </div>
     <div className="drop-shadow-md border-gray-300 border-2">
      {activityData && totalActivity &&  <BarChart totalActivity={totalActivity} activityData={activityData} />}
---
+++ src/components/DayWiseActivityChart.tsx
@@ -0,0 +1,103 @@
+import { DayWiseActivity, Items, RepoActions } from "@/global.types";
+import { useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
+import SelectActionTypes from "./SelectActionTypes";
+
+const margin = { top: 20, right: 30, bottom: 55, left: 70 };
+const width = 640, height = 300;
+
+const findRepoActionsByDate = (dayWiseActivity: DayWiseActivity[], actionType: RepoActions) => {
+  return dayWiseActivity.map(data => {
+    const openPRs = data.items.children.find(i => i.label === actionType) as Items;
+    return ({ date: data.date, ...openPRs})
+  })
+}
+
+
+const DayWiseActivityChart = ({dayWiseActivity} : {dayWiseActivity: DayWiseActivity[]}) => {
+  const dayChartRef = useRef(null);
+  console.log(dayWiseActivity)
+  const [selectActionType, setSelectActionType] = useState(RepoActions.PR_OPEN);
+  const openPRsByDate = findRepoActionsByDate(dayWiseActivity, selectActionType);
+  console.log(openPRsByDate);
+
+  useEffect(() => {
+    // const x = d3.scaleBand().domain(itr).range([0, 960]).padding(0.1);
+
+    const xMin = d3.min(openPRsByDate, (d) => d.date) as Date;
+    const xMax = d3.max(openPRsByDate, (d) => d.date) as Date;
+
+    const yMin = d3.min(openPRsByDate, (d) => d.count) as string;
+    const yMax = d3.max(openPRsByDate, (d) => d.count) as string;
+
+
+    const xScale = d3.scaleTime()
+      .domain([new Date(xMin), new Date(xMax)])
+      .range([margin.left, width-margin.right])
+
+    const yScale = d3.scaleLinear()
+      .domain([Number(yMin), Number(yMax)])
+      .range([height-margin.bottom, margin.top])
+
+
+    // 4. Draw and transform/translate horizontal and vertical axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    const svg = d3.select(dayChartRef.current).attr("width", width).attr("height", height).attr("viewBox", [0, 0, width, height]);
+
+
+    svg.append("g")
+    .attr("transform", "translate(0, "+ (height - margin.bottom) + ")")
+    .attr("id", "x-axis")
+    .call(xAxis)
+
+    svg.append("g")
+        .attr("transform", "translate("+ (margin.left)+", 0)")
+        .attr("id", "y-axis")
+        .call(yAxis)
+    // 5. Draw individual bars and define mouse events for the tooltip
+    const barwidth = (xScale.range()[1] - xScale.range()[0]) / openPRsByDate.length
+
+    const tooltip = d3.select("#main").append("div").attr("id", "tooltip").style("visibility", "hidden")
+
+    svg.selectAll("rect")
+    .data(openPRsByDate)
+    .enter()
+    .append("rect")
+    .attr("x", (d) => xScale(new Date(d.date)))
+    .attr("y", (d) => yScale(+d.count))
+    .attr("width", barwidth)
+    .attr("height", (d) => height - margin.bottom - yScale(+d.count))
+    .attr("fill", d => d.fillColor)
+    .attr("data-date", (d) => d.date as unknown as string)
+    .attr("data-count", (d) => d.count)
+    .on("mouseover", (event, d) => {
+        tooltip.style("visibility", "visible")
+               .style("left", event.pageX+10+"px")
+               .style("top", event.pageY-80+"px")
+               .attr("data-date", d.date as unknown as string)
+               .html(d.date + "</br>" + d.count + " " + d.label )
+    })
+    .on("mousemove", (event) => {
+        tooltip.style("left", event.pageX+10+"px")
+    })
+    .on("mouseout", () => {
+        tooltip.style("visibility", "hidden")
+    })
+
+    // return () => dayChartRef.current = null;
+  
+
+  }, [selectActionType])
+
+
+  return (<div id="main">
+    <SelectActionTypes setSelectActionTypes={setSelectActionType} />
+
+    <svg id="daywise" ref={dayChartRef} ></svg>
+  </div>)
+
+}
+
+export default DayWiseActivityChart;
